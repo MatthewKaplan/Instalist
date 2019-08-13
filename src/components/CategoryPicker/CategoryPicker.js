68746b390b1bc28734b625b134652cd6d8ddbb4f
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-13 12:36:47 -0600

    Style category buttons when active

--- src/components/CategoryPicker/CategoryPicker.js
+++ src/components/CategoryPicker/CategoryPicker.js
@@ -1,65 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons, MaterialCommunityIcons, FontAwesome, Feather } from '@expo/vector-icons';
 import { styles } from './styles';
 
 export class CategoryPicker extends Component {
+	state = { chosenCategory: '' };
 	render () {
-		const { categoryContainer, textStyle, categoryStyles } = styles;
+		const { categoryContainer, textStyle, categoryStyles, iconStyles, iconBackground, elecActive, activeText, homeActive, fashActive, carsActive, partsActive, sportsActive, babyActive, headPhonesActive  } = styles;
+		const { chosenCategory } = this.state;
+		console.log(this.state.chosenCategory);
 		return (
 			<React.Fragment>
 				<View style={categoryContainer}>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(107, 206, 187)', width: 70, borderRadius: 55, height: 70 }}>
-							<Ionicons name="ios-phone-portrait" color="#fff" size={55} style={{ alignSelf: 'center' }} />
+					<TouchableOpacity
+						style={chosenCategory === 'electronics' ? elecActive : categoryStyles}
+						onPress={() => this.setState({ chosenCategory: 'electronics' })}>
+						<View style={{ backgroundColor: 'rgb(107, 206, 187)', ...iconBackground }}>
+							<Ionicons name="ios-phone-portrait" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Electronics</Text>
-					</View>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(255, 210, 0)', width: 70, borderRadius: 55, height: 70 }}>
-							<MaterialCommunityIcons name="sofa" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'electronics' ? activeText : textStyle}>Electronics</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'home' ? homeActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'home' })}>
+						<View style={{ backgroundColor: 'rgb(255, 210, 0)', ...iconBackground }}>
+							<MaterialCommunityIcons name="sofa" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Home and Garden</Text>
-					</View>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(253, 125, 145)', width: 70, borderRadius: 55, height: 70 }}>
-							<MaterialCommunityIcons name="shoe-heel" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'home' ? activeText : textStyle}>Home and Garden</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'fashion' ? fashActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'fashion' })}>
+						<View style={{ backgroundColor: 'rgb(253, 125, 145)', ...iconBackground }}>
+							<MaterialCommunityIcons name="shoe-heel" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Fashion and Accessories</Text>
-					</View>
-
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(50, 127, 182)', width: 70, borderRadius: 55, height: 70 }}>
-							<FontAwesome name="car" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'fashion' ? activeText : textStyle}>Fashion and Accessories</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'cars' ? carsActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'cars' })}>
+						<View style={{ backgroundColor: 'rgb(50, 127, 182)', ...iconBackground }}>
+							<FontAwesome name="car" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Cars</Text>
-					</View>
+						<Text style={chosenCategory === 'cars' ? activeText : textStyle}>Cars</Text>
+					</TouchableOpacity>
 				</View>
+
 				<View style={categoryContainer}>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(251, 134, 54)', width: 70, borderRadius: 55, height: 70 }}>
-							<MaterialCommunityIcons name="motorbike" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+					<TouchableOpacity style={chosenCategory === 'parts' ? partsActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'parts' })}>
+						<View style={{ backgroundColor: 'rgb(251, 134, 54)', ...iconBackground }}>
+							<MaterialCommunityIcons name="motorbike" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Other Vehicles and Parts</Text>
-					</View>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(163, 206, 113)', width: 70, borderRadius: 55, height: 70 }}>
-							<FontAwesome name="soccer-ball-o" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'parts' ? activeText : textStyle}>Other Vehicles and Parts</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'sports' ? sportsActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'sports' })}>
+						<View style={{ backgroundColor: 'rgb(163, 206, 113)', ...iconBackground }}>
+							<FontAwesome name="soccer-ball-o" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Sports, Leisure and Games</Text>
-					</View>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(32, 195, 243)', width: 70, borderRadius: 55, height: 70 }}>
-							<MaterialCommunityIcons name="baby-buggy" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'sports' ? activeText : textStyle}>Sports, Leisure and Games</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'baby' ? babyActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'baby' })}>
+						<View style={{ backgroundColor: 'rgb(32, 195, 243)', ...iconBackground }}>
+							<MaterialCommunityIcons name="baby-buggy" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Baby and Child</Text>
-					</View>
-					<View style={categoryStyles}>
-						<View style={{ backgroundColor: 'rgb(236, 126, 189)', width: 70, borderRadius: 55, height: 70 }}>
-							<Feather name="headphones" color="#fff" size={55} style={{ alignSelf: 'center' }}/>
+						<Text style={chosenCategory === 'baby' ? activeText : textStyle}>Baby and Child</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={chosenCategory === 'movies' ? headPhonesActive : categoryStyles} onPress={() => this.setState({ chosenCategory: 'movies' })}>
+						<View style={{ backgroundColor: 'rgb(236, 126, 189)', ...iconBackground }}>
+							<Feather name="headphones" color="#fff" size={55} style={iconStyles} />
 						</View>
-						<Text style={textStyle}>Movies, Books, and Music</Text>
-					</View>
+						<Text style={chosenCategory === 'movies' ? activeText : textStyle}>Movies, Books, and Music</Text>
+					</TouchableOpacity>
 				</View>
 			</React.Fragment>
 		);
--- src/components/CategoryPicker/styles.js
+++ src/components/CategoryPicker/styles.js
@@ -7,12 +7,89 @@ export const styles = StyleSheet.create({
 	categoryContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-around',
-		height: '38%'
+		height: '42.5%'
 	},
 	categoryStyles: {
 		width: '25%',
-    alignItems: 'center',
-		borderWidth: .25,
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	iconStyles: {
+		alignSelf: 'center'
+	},
+	iconBackground: {
+		width: 75,
+		borderRadius: 55,
+		height: 75,
+		justifyContent: 'center'
+	},
+	elecActive: {
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		backgroundColor: 'rgb(107, 206, 187)'
+	},
+	activeText: {
+		color: 'white',
+		textAlign: 'center'
+	},
+	homeActive: {
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		backgroundColor: 'rgb(255, 210, 0)'
+	},
+	fashActive: {
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		backgroundColor: 'rgb(253, 125, 145)'
+	},
+	carsActive: {
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		backgroundColor: 'rgb(50, 127, 182)'
+	},
+	headPhonesActive: {
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		backgroundColor: 'rgb(236, 126, 189)'
+	},
+	babyActive: {
+		backgroundColor: 'rgb(32, 195, 243)',
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	sportsActive: {
+		backgroundColor: 'rgb(163, 206, 113)',
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	partsActive: {
+		backgroundColor: 'rgb(251, 134, 54)',
+		width: '25%',
+		alignItems: 'center',
+		borderWidth: 0.25,
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	}

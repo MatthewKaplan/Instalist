b9341dc98bb366e207f3dd56df9cfb373acc64b6
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-13 05:10:53 -0600

    Map title

--- src/components/MapScreen/MapScreen.js
+++ src/components/MapScreen/MapScreen.js
@@ -5,8 +5,6 @@ import MapView from 'react-native-maps';
 
 const { width, height } = Dimensions.get('window');
 
-const SCREEN_HEIGHT = height;
-const SCREEN_WIDTH = width;
 const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
@@ -22,53 +20,55 @@ export class MapScreen extends Component {
 			latitude: 0,
 			longitude: 0
 		}
-  };
-  
-	componentDidMount () {
-		navigator.geolocation.getCurrentPosition((position) => {
-			let lat = parseFloat(position.coords.latitude)
-			let long = parseFloat(position.coords.longitude)
+	};
 
-			let initialRegion = {
+	componentDidMount () {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				let lat = parseFloat(position.coords.latitude);
+				let long = parseFloat(position.coords.longitude);
+
+				let initialRegion = {
+					latitude: lat,
+					longitude: long,
+					latitudeDelta: LATITUDE_DELTA,
+					longitudeDelta: LONGITUDE_DELTA
+				};
+
+				this.setState({ initialPosition: initialRegion });
+				this.setState({ markerPosition: initialRegion });
+			},
+			error => alert(JSON.stringify(error)),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+		);
+
+		this.watchID = navigator.geolocation.watchPosition(position => {
+			let lat = parseFloat(position.coords.latitude);
+			let long = parseFloat(position.coords.longitude);
+
+			let lastRegion = {
 				latitude: lat,
 				longitude: long,
 				latitudeDelta: LATITUDE_DELTA,
 				longitudeDelta: LONGITUDE_DELTA
-      }
-      
-			this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-      (error) => alert(JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+			};
 
-      this.watchID = navigator.geolocation.watchPosition((position) => {
-        let lat = parseFloat(position.coords.latitude)
-        let long = parseFloat(position.coords.longitude)
+			this.setState({ initialPosition: lastRegion });
+			this.setState({ markerPosition: lastRegion });
+		});
+	}
 
-        let lastRegion = {
-          latitude: lat,
-          longitude: long,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA
-        }
-
-        this.setState({initialPosition: lastRegion})
-        this.setState({markerPosition: lastRegion})
-      })
-    };
-    
-    componentWillUnmount() {
-      navigator.geolocation.clearWatch(this.watchID)
-    }
+	componentWillUnmount () {
+		navigator.geolocation.clearWatch(this.watchID);
+	}
 
 	render () {
 		return (
 			<View style={{ flex: 1 }}>
-				<NavDrawer>
-          <MapView region={this.state.initialPosition} style={{ flex: 1 }}>
-            <MapView.Marker coordinate={this.state.markerPosition}></MapView.Marker>
-          </MapView>
+				<NavDrawer name="Map">
+					<MapView region={this.state.initialPosition} style={{ flex: 1 }}>
+						<MapView.Marker coordinate={this.state.markerPosition} />
+					</MapView>
 				</NavDrawer>
 			</View>
 		);

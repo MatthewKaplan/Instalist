2e50a9af5f69c05a6a55b055755940bf30e63668
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-12 23:44:12 -0600

    Route user to homescreen when signed in

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 import SplashPage from './components/SplashPage/SplashPage';
+import HomeScreen from './components/HomeScreen/HomeScreen';
 
 const RouterComponent = () => {
 	return (
@@ -10,6 +11,7 @@ const RouterComponent = () => {
 			<Router>
 				<Scene key="root" panHandlers={null}>
 					<Scene key="splashPage" component={SplashPage} hideNavBar={true} />
+					<Scene key="homeScreen" component={HomeScreen} hideNavBar={true} />
 				</Scene>
 			</Router>
 		</React.Fragment>

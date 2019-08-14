2e1987e0f06230fcf89c5c35e6930675e9233ad3
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-13 18:54:53 -0600

    Action handlers for user item img, cat,title, $

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,26 @@
 export * from './auth_actions';
+
+export const setItemImage = text => ({
+	type: 'SET_ITEM_IMAGE',
+	text
+});
+
+export const setItemCategory = text => ({
+	type: 'SET_ITEM_CATEGORY',
+	text
+});
+
+export const setItemTitle = text => ({
+	type: 'SET_ITEM_TITLE',
+	text
+});
+
+export const setItemDescription = text => ({
+	type: 'SET_ITEM_DESCRIPTION',
+	text
+});
+
+export const setItemPrice = text => ({
+	type: 'SET_ITEM_PRICE',
+	text
+});
